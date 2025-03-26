d5c5b3e74dd94535a9651f1c2b538a4811755622
lea <115721978+liodice222@users.noreply.github.com>
2025-03-26 10:31:23 -0400

    Create variables.tf

---
+++ oci_events_rule/variables.tf
@@ -0,0 +1,29 @@
+variable "rule_condition" {
+  description = "event types for rule conditions"
+}
+
+variable "action_type" {
+  description = "action in response to event trigger"
+}
+
+variable "tenancy_ocid" {
+    description = "Nacaus 19e tenancy ocid"
+}
+
+variable "user_ocid" {
+    description = "Lea's user ocid"
+}
+
+variable "fingerprint" {
+        description = "Lea's fingerprint"
+}
+
+variable "private_key_path" {
+    description = "path to private key"
+}
+
+variable "compartment_id" {
+    description = "LeaTesting compartment ocid"
+}
+
+variable "notification_endpoint" {description = "admin email"}
