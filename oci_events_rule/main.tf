92945514a0e6520182346f35793ac887178b2404
lea <115721978+liodice222@users.noreply.github.com>
2025-03-26 10:34:19 -0400

    Update main.tf
    added variables event_condition and action_type

--- oci_events_rule/main.tf
+++ oci_events_rule/main.tf
@@ -11,29 +11,43 @@ provider "oci" {
         region = var.region
 }
 
+
+resource "oci_ons_notification_topic" "test_topic" {
+        name = "test_ons_topic_tf"
+        compartment_id = var.compartment_id
+        description = "notification topic"
+
+}
+
+resource "oci_ons_subscription" "test_subscription" {
+        topic_id = oci_ons_notification_topic.test_topic.id
+        protocol = "EMAIL"
+        endpoint = var.notification_endpoint
+        compartment_id = var.compartment_id
+}
+
+
+#oci_events_rule
 resource "oci_events_rule" "test_rules" {
-	Compartment_id = var.compartment_id
-	description = 
-	is_enabled = true
-	actions {
-		actions {
-			Action_type = "ONS"
-			description = "send notification"
-			Is_enabled = true
-			Topic_id = 
-		
-			}
-		}
-	condition {
-		event_type = [
-			"com.oraclecloud.dataflow.createapplication",
-			"com.oraclecloud.vmware.createclustertask.begin", 
-			"com.oraclecloud.oic.createintegrationinstance.begin",
-			"com.oraclecloud.oic.createworkspace.begin",
-			"com.oraclecloud.exacc.createcloudexadatainfrastrcture.begin"
-			]
-		}
-	
+        compartment_id = var.compartment_id
+        description = "test event rules creation"
+        is_enabled = true
+        display_name = "test event rule creation"
+        actions {
+                actions {
+                        action_type = var.action_type
+                        description = "send notification"
+                        is_enabled = true
+                        topic_id = oci_ons_notification_topic.test_topic.id
+
+                        }
+                }
+        condition = var.rule_condition
+}
+
+ # version requirement
+
+terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
