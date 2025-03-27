9ac56119ba30b2989217ed51d36f138763babf30
lea <115721978+liodice222@users.noreply.github.com>
2025-03-27 08:47:53 -0400

    Update main.tf
    edited names to have no spaces 
    made sure topic_ids match

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-# combined .tf file
 provider "oci" {
 
         tenancy_ocid = var.tenancy_ocid
@@ -12,15 +11,15 @@ provider "oci" {
         region = "us-ashburn-1"
 }
 
-resource "oci_ons_notification_topic" "Notification Topic" {
-        name = "test_ons_topic_tf"
+resource "oci_ons_notification_topic" "Notification_Topic" {
+        name = "admin_ons_topic"
         compartment_id = var.compartment_id
-        description = "notification topic"
+        description = "notification topic for events rules"
 
 }
 
-resource "oci_ons_subscription" "Admin Subscription" {
-        topic_id = oci_ons_notification_topic.notification_topic.id
+resource "oci_ons_subscription" "Admin_Subscription" {
+        topic_id = oci_ons_notification_topic.admin_ons_topic.id
         protocol = "EMAIL"
         endpoint = var.notification_endpoint
         compartment_id = var.compartment_id
@@ -28,9 +27,9 @@ resource "oci_ons_subscription" "Admin Subscription" {
 
 
 #oci_events_rule
-resource "oci_events_rule" "Event Rules" {
+resource "oci_events_rule" "Event_Rules" {
         compartment_id = var.compartment_id
-        description = "Event Rules including OIC, Exadata, VMware"
+        description = "Event Rules including OIC, Exadata, Dataflow, VMware"
         is_enabled = true
         display_name = "Event Rules "
         actions {
@@ -38,7 +37,7 @@ resource "oci_events_rule" "Event Rules" {
                         action_type = var.action_type
                         description = "send notification to admin"
                         is_enabled = true
-                        topic_id = oci_ons_notification_topic.notification_topic.id
+                        topic_id = oci_ons_notification_topic.admins_ons_topic.id
 
                         }
                 }
