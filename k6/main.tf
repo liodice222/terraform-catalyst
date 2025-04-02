a8b2c7068294dee63d1d673b910fce800a3a744b
lea <115721978+liodice222@users.noreply.github.com>
2025-04-02 13:50:23 -0400

    Create main.tf
    outline for main.tf

---
+++ k6/main.tf
@@ -0,0 +1,10 @@
+# creation steps
+# OCI image
+# VCN
+# IG 
+# RT
+# public subnet
+# private subnet? 
+# secuirty list - open ports for grafana and Flask, incoming ssh connections tcp 6, open all egress 
+# flask app instance 
+# grafana instance
